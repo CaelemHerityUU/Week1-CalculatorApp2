3ce2d65b6ea5faa66f1f4c966601b5115f0b2ab0
Caelem Herity <Herity-C1@ulster.ac.uk>
2024-09-24 14:57:55 +0100

    Add project files.

---
+++ CalculatorRedone/Program.cs
@@ -0,0 +1,17 @@
+﻿// Variables names should be short yet meaningful.
+
+//This integer variable stores the first number
+//Ask the user to input the first number
+Console.WriteLine("Type in the first number followed by the Enter Key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+//Variable stores the second number
+//Ask the user to input the second number.
+Console.WriteLine("Type the second number followed by the Enter Key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+//The "result" is the addition of firstNumber and secondNumber
+int result = firstNumber + secondNumber;
+
+//Output the answer to the console
+Console.WriteLine("Adding {0} and {1} give the answer {2}", firstNumber, secondNumber, result);
